7ca837b32cab3c39244899bcd8ac892005559df8
Sirwagya Shekhar <sirwagya7@gmail.com>
2026-08-27 11:40:43 +0530

    fixed the file map

--- BackendJS/index.js
+++ BackendJS/index.js
@@ -36,8 +36,6 @@ app.post('/signUp', async (req, res)=>{
 app.post('/signin', async (req, res)=>{
     let {email, password} = req.body;
     let mail = await userModel.findOne({email})
-    console.log(mail);
-    //password = await bcrypt.hash(password, 10);
 
     if (mail){
         await bcrypt.compare(password, mail.password).then((match)=>{
